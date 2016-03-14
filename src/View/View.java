4aea2845cb5d0795d963767e893f50dd91c86171
cassie_hanyu <cassie_hanyu@hotmail.com>
2016-03-14 04:13:02 -0400

    almost done

--- src/View/View.java
+++ src/View/View.java
@@ -21,6 +21,8 @@ public class View extends JPanel{
     private int width = 625;
     private int height = 580;
 
+    private int imageDisplayed;
+
     public View(Model model){
         this.model = model;
         this.imagePanelList = new ArrayList<>();
@@ -60,9 +62,15 @@ public class View extends JPanel{
                             View.this.add(Box.createVerticalStrut(10));
                         }
                     }
-                    
-                    View.this.validate();
+                    View.this.setPreferredSize(getViewPreferredSize(indexes.size()));
+//                    View.this.setSize(View.this.getPreferredSize());
+
+//                    View.this.revalidate();
+//                    View.this.invalidate();
+//                    View.this.invalidate();
+                    imageDisplayed = indexes.size();
                     View.this.repaint();
+                    View.this.updateUI();
                 }else if(model.isFilterRemoved() && !model.isFilterSelected()){
                     View.this.removeAll();
                     for(int i = 0; i < model.getTotalImage(); i++){
@@ -76,13 +84,22 @@ public class View extends JPanel{
                             View.this.add(Box.createVerticalStrut(10));
                         }
                     }
-
-                    View.this.validate();
+                    View.this.setPreferredSize(getViewPreferredSize(model.getTotalImage()));
+                    imageDisplayed = model.getTotalImage();
+                    View.this.revalidate();
                     View.this.repaint();
+                    View.this.updateUI();
                 }
                 else {
                     addImages();
-                    View.this.validate();
+                    View.this.setPreferredSize(getViewPreferredSize(imagePanelList.size()));
+                    imageDisplayed = imagePanelList.size();
+//                    View.this.setPreferredSize(new Dimension(600,900));
+                    View.this.revalidate();
+//                    View.this.validate();
+//                    View.this.invalidate();
+                    View.this.repaint();
+                    View.this.updateUI();
                 }
             }
         });
@@ -95,8 +112,7 @@ public class View extends JPanel{
             @Override
             public void componentResized(ComponentEvent e) {
                 System.out.println("View width: " + View.this.getWidth() + "   View Height: " + View.this.getHeight());
-
-
+                View.this.setPreferredSize(getViewPreferredSize(View.this.imageDisplayed));
             }
         });
     }
@@ -109,7 +125,9 @@ public class View extends JPanel{
         empty = BorderFactory.createEmptyBorder(0,0,0,0);
         this.setBorder(empty);
         addImages();
-        this.validate();
+        this.setPreferredSize(getViewPreferredSize(model.getTotalImage()));
+        imageDisplayed = model.getTotalImage();
+        this.revalidate();
         this.repaint();
     }
 
@@ -120,7 +138,9 @@ public class View extends JPanel{
         empty = BorderFactory.createEmptyBorder(10,10,0,10);
         this.setBorder(empty);
         addImages();
-        this.validate();
+        this.setPreferredSize(getViewPreferredSize(model.getTotalImage()));
+        imageDisplayed = model.getTotalImage();
+        this.revalidate();
         this.repaint();
     }
 
@@ -135,6 +155,39 @@ public class View extends JPanel{
                 View.this.add(Box.createVerticalStrut(10));
             }
         }
+    }
+
+    private Dimension getViewPreferredSize(int totalItem){
+        int width = this.width-10;
+        int height = this.height-10;
+        int newWidth, newHeight;
+        int panelWidth, panelHeight;
+        double item;
+        int itemInRow;
+        if(model.getCurLayout() == Layout.GRIDLAYOUT){
+            panelWidth = 195 + 10;
+            panelHeight = 180 + 10;
+
+            item= (double)width/(double)panelWidth;
+            itemInRow = (int) Math.floor(item);
+            if(item - itemInRow > 0.5){
+                itemInRow++;
+            }
+
+            newWidth = (itemInRow * panelWidth + 10);
+            newWidth = (newWidth < this.width)? this.width : newWidth;
+
+            newHeight = (int) Math.ceil((double)totalItem/(double) itemInRow) * panelHeight + 10;
+            newHeight = (newHeight < this.height)? this.height : newHeight;
+        }else{
+            panelWidth = 500+10;
+            panelHeight = 150+10;
+            newWidth = this.width;
+            newHeight = panelHeight * totalItem + 10;
+        }
+        int test = this.getWidth();
+        int test2 = this.getHeight();
+        return new Dimension(newWidth, newHeight);
     }
 
 }
